47382c42e1bb60a156aff5f0b90fa1f174197c01
MuhammadZohaibAwan <iamzohaibmuhammad@gmail.com>
2023-04-06 04:08:39 +0500

    404-redirect-added

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ const shopRoutes = require('./routes/shop');
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+    //here res.status or res.any method can be send but .send() should be last sent call .
+})
+
 // comment for checking updated pushing email on git account 
 app.listen(3000);
